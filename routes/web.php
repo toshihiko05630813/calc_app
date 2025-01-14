db69c9f1df59428cc02550615e7eaa4e5c71e566
toshihiko05630813 <atabihs00410okihihsot0813@gmail.com>
2025-01-14 20:06:46 +0900

    計算アプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use App\Http\Controllers\CalcController;
 use Brick\Math\Internal\Calculator;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/calcs/{value1}/{operator}/{value2}', [CalcController::class, 'result']);
